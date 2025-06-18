864cde1d99906bbad84f8315153e664bbab7b7cc
Heathcliff <jtplayer2206@gmail.com>
2025-06-18 22:50:46 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0, overflow: 'hidden' }}>
       <iframe
-        src="http://101.35.252.91:9091/"
+        src="http://101.35.252.91:9091"
         style={{ width: "100%", height: "100%", border: "none" }}
       />
     </div>
