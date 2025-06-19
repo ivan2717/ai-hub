2213de52fa6ae8558d1e419a6ccb29d534581bbd
Heathcliff <jtplayer2206@gmail.com>
2025-06-19 16:00:48 +0800

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useStore } from '../store';
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
+  component: Home,
+});
 
-function RouteComponent() {
-  return <div>Hello "/"!</div>
+function Home() {
+
+  const navigate = useNavigate();
+  const {setExplorerUrl} = useStore()
+
+  const handleClick = (url:string) => {
+    setExplorerUrl(url)
+    navigate({ to: '/explorer' });
+  };
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0, overflow: 'hidden' }}>
+      <div onClick={()=>handleClick("http://101.35.252.91:9092")} style={{}}>
+        AI 3D
+      </div>
+      <div onClick={()=>handleClick("http://101.35.252.91:9091") }>
+        AI 绘画
+      </div>
+    </div>
+  );
 }
