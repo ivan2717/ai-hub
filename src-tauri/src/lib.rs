9c6521b9d094310feeb07c6907fc610004395358
Heathcliff <jtplayer2206@gmail.com>
2025-06-19 16:34:50 +0800

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,7 +17,7 @@ pub fn run() {
             // 命令窗口进入全屏模式
             // 这将在保持无装饰和透明设置的同时，使窗口全屏
             window
-                .set_fullscreen(false)
+                .set_fullscreen(true)
                 .expect("Failed to set window to fullscreen");
 
             Ok(())
@@ -27,15 +27,15 @@ pub fn run() {
             // 如果用户试图通过系统快捷键退出全屏
             WindowEvent::Resized(..) => {
                 // 立即强制其返回全屏状态
-                // let _ = window.set_fullscreen(true);
+                let _ = window.set_fullscreen(true);
             }
             // 阻止通过 Alt+F4 或 Cmd+Q 关闭
             WindowEvent::CloseRequested { api, .. } => {
-                // api.prevent_close();
+                api.prevent_close();
             }
             // 确保窗口始终在最前和获取焦点 (Kiosk模式加强)
             WindowEvent::Focused(false) => {
-                // let _ = window.set_focus();
+                let _ = window.set_focus();
             }
             _ => {}
         })
