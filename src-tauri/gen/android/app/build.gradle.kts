a8a37c90457ee662a5f8b01fc47809a8267030a8
Heathcliff <jtplayer2206@gmail.com>
2025-06-20 17:07:21 +0800

    安卓打包配置

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -23,6 +23,9 @@ android {
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+        }
     }
     buildTypes {
         getByName("debug") {
